718c55f3efe807932bff1ebc1081c4220cf90f7d
Winsor Jeremiah <158396519+walpuerto@users.noreply.github.com>
2024-02-22 11:44:45 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SQL Integration - ALPUERTO"
+include(":app")
+ 
